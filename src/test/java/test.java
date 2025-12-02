0a198d1fbe7862c518b147809e240963c32da529
Maryna Huzenko <marina.guzenko@gmail.com>
2025-12-02 15:39:28 -0700

    person2commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,9 +1,9 @@
 public class test {
     public static void main(String[] args) {
 
-        System.out.println("this is the project version 05");
+        System.out.println("this is the project version 6 made by person 2");
 
 
     }
 }
-// done with version five
+// done with version six
